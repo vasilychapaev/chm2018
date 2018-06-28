90eb47a18328ccb710d2ad5cf508199fc34c06cf
Vasily Chapaev <vasilychapaev@gmail.com>
2018-06-28 10:38:00 +0300

    search-autocomplete prod

--- ajaxCountries.php
+++ ajaxCountries.php
@@ -6,7 +6,15 @@ Lms_Application::prepareApi();
 Lms_Debug::debug('Request URI: ' . $_SERVER['REQUEST_URI']);
 
 $db = Lms_Db::get('main');
-$items = $db->select('select name_ru id, name_ru text from chm_commands');
-
+//$items = $db->select('select name_ru id, name_ru text from chm_commands');
+//echo json_encode(['results'=>$items], JSON_UNESCAPED_UNICODE);
 //echo '<pre>';print_r($items);
-echo json_encode(['results'=>$items]);
+
+
+$items = $db->select('select name_ru, flag_img from chm_commands order by name_ru');
+$tmp = [];
+foreach ($items as $v)
+    $tmp[] = array_values($v);
+//echo '<pre>';print_r($tmp);
+echo json_encode($tmp, JSON_UNESCAPED_UNICODE);
+
